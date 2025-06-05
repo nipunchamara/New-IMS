9400edb0f2742c6b6bd083ce9aa1979ec7210c19
nipunchamara <83004320+nipunchamara@users.noreply.github.com>
2025-06-05 20:09:47 +0530

    starter-project

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+      console.log(process.env.MONGO_URI);
+    });
+  })
+  .catch(err => console.log(err));
